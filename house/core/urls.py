6c597067afa17a93ebb8d2bb447faa213776632b
atmel <kolia1985@gmail.com>
2010-03-06 22:21:55 +0200

    add floor page

--- house/core/urls.py
+++ house/core/urls.py
@@ -5,4 +5,6 @@ urlpatterns = patterns('core.views',
     url(r'^$', 'home', name='home'),
     url(r'^project/(?P<project_slug>[^/]+)/$', 'project', \
         name='project'),
+    url(r'^floor/(?P<id>\d+)/$', 'floor', \
+        name='floor'),
 )
--- house/core/views.py
+++ house/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template import RequestContext
 from django.utils.translation import ugettext as _
 
-from core.models import Building
+from core.models import Building, Floor
 from sortedtable.models import AjaxTable
 
 def home(request, template='core/index.html'):
@@ -27,4 +27,9 @@ def project(request, project_slug, template='core/project.html'):
     floor_table = AjaxTable(building.floors.all(), fields,'core/project_table.html' )
     context = RequestContext(request, {"building": building,
                                        "floor_table": floor_table.render(request),})
+    return render_to_response(template, context)
+
+def floor(request, id, template='core/floor.html'):
+    floor = get_object_or_404(Floor, pk=id)
+    context = RequestContext(request, {"floor": floor,})
     return render_to_response(template, context)
